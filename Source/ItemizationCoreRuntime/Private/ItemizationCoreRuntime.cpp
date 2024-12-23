2f84ed4c70e92bd289a3259efe328e8a62c6417f
MajorTomAW <tom.wrnr.private@gmail.com>
2024-12-23 23:54:08 +0100

    Runtime Module

---
+++ Source/ItemizationCoreRuntime/Private/ItemizationCoreRuntime.cpp
@@ -0,0 +1,50 @@
+﻿#include "ItemizationCoreRuntime.h"
+
+#include "GameplayDebugger.h"
+#include "ActorComponents/InventoryManager.h"
+#include "GameFramework/HUD.h"
+#include "GameplayDebugger/GameplayDebuggerCategory_Itemization.h"
+
+#define LOCTEXT_NAMESPACE "FItemizationCoreRuntimeModule"
+
+class FItemizationCoreRuntimeModule : public IItemizationCoreRuntimeModule
+{
+public:
+	//~ Begin IModuleInterface Interface
+	virtual void StartupModule() override;
+	virtual void ShutdownModule() override;
+	//~ End IModuleInterface Interface
+};
+IMPLEMENT_MODULE(FItemizationCoreRuntimeModule, ItemizationCoreRuntime)
+
+
+
+
+
+void FItemizationCoreRuntimeModule::StartupModule()
+{
+#if WITH_GAMEPLAY_DEBUGGER
+	IGameplayDebugger& GameplayDebugger = IGameplayDebugger::Get();
+	GameplayDebugger.RegisterCategory("Itemization", IGameplayDebugger::FOnGetCategory::CreateStatic(&FGameplayDebuggerCategory_Itemization::MakeInstance));
+	GameplayDebugger.NotifyCategoriesChanged();
+#endif
+
+	if (!IsRunningDedicatedServer())
+	{
+		AHUD::OnShowDebugInfo.AddStatic(&UInventoryManager::OnShowDebugInfo);
+	}
+}
+
+void FItemizationCoreRuntimeModule::ShutdownModule()
+{
+#if WITH_GAMEPLAY_DEBUGGER
+	if (IGameplayDebugger::IsAvailable())
+	{
+		IGameplayDebugger& GameplayDebugger = IGameplayDebugger::Get();
+		GameplayDebugger.UnregisterCategory("Itemization");
+		GameplayDebugger.NotifyCategoriesChanged();
+	}
+#endif
+}
+
+#undef LOCTEXT_NAMESPACE
---
+++ Source/ItemizationCoreRuntime/Public/ItemizationCoreRuntime.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Modules/ModuleManager.h"
+
+class IItemizationCoreRuntimeModule : public IModuleInterface
+{
+public:
+    /**
+     * Singleton-like access to this module's interface. This is just for convenience!
+     * Beware of calling this during the shutdown phase, though. Your module might have been unloaded already.
+     *
+     * @returns Singleton instance, loading the module on demand if needed
+     */
+	static IItemizationCoreRuntimeModule& Get()
+	{
+		return FModuleManager::LoadModuleChecked<IItemizationCoreRuntimeModule>("ItemizationCoreRuntime");
+	}
+};
