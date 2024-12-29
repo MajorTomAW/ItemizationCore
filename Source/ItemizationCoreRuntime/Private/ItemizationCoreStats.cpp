8a9fb95e1d0bb2fc28c5b6f7b6beb42d5de238b6
MajorTomAW <tom.wrnr.private@gmail.com>
2024-12-29 14:53:27 +0100

    Equipment Stats

--- Source/ItemizationCoreRuntime/Private/ItemizationCoreStats.cpp
+++ Source/ItemizationCoreRuntime/Private/ItemizationCoreStats.cpp
@@ -5,6 +5,7 @@
 
 
 DEFINE_STAT(STAT_FindItemEntryFromHandle);
+DEFINE_STAT(STAT_FindEquipmentEntryFromHandle);
 DEFINE_STAT(STAT_EvaluateCurrentItemContext);
 DEFINE_STAT(STAT_CanCreateNewStack);
 DEFINE_STAT(STAT_CanCombineItemEntries);
--- Source/ItemizationCoreRuntime/Public/ItemizationCoreStats.h
+++ Source/ItemizationCoreRuntime/Public/ItemizationCoreStats.h
@@ -8,6 +8,7 @@
 DECLARE_STATS_GROUP(TEXT("ItemizationCore"), STATGROUP_ItemizationCore, STATCAT_Advanced);
 
 DECLARE_CYCLE_STAT_EXTERN(TEXT("Find Item Entry From Handle"), STAT_FindItemEntryFromHandle, STATGROUP_ItemizationCore, );
+DECLARE_CYCLE_STAT_EXTERN(TEXT("Find Equipment Entry From Handle"), STAT_FindEquipmentEntryFromHandle, STATGROUP_ItemizationCore, );
 DECLARE_CYCLE_STAT_EXTERN(TEXT("Evaluate Current Item Context"), STAT_EvaluateCurrentItemContext, STATGROUP_ItemizationCore, );
 DECLARE_CYCLE_STAT_EXTERN(TEXT("Can Create New Stack"), STAT_CanCreateNewStack, STATGROUP_ItemizationCore, );
 DECLARE_CYCLE_STAT_EXTERN(TEXT("Can Combine Item Entries"), STAT_CanCombineItemEntries, STATGROUP_ItemizationCore, );
