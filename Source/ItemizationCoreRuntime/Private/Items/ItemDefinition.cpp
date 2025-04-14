db659620f4ae992fc882becd2002239e467e6074
MajorTomAW <tom.wrnr.private@gmail.com>
2025-04-14 21:01:30 +0200

    Added some default /common properties to the UItemDefinition

--- Source/ItemizationCoreRuntime/Private/Items/ItemDefinition.cpp
+++ Source/ItemizationCoreRuntime/Private/Items/ItemDefinition.cpp
@@ -2,6 +2,11 @@
 
 
 #include "Items/ItemDefinition.h"
+#include "UObject/AssetRegistryTagsContext.h"
+
+#if WITH_EDITOR
+#include "UObject/ObjectSaveContext.h"
+#endif
 
 #include UE_INLINE_GENERATED_CPP_BY_NAME(ItemDefinition)
 
@@ -10,6 +15,9 @@
 UItemDefinition::UItemDefinition(const FObjectInitializer& ObjectInitializer)
 	: Super(ObjectInitializer)
 {
+#if WITH_EDITORONLY_DATA
+	AssetType_Reg = AssetType.AssetTypeId;
+#endif
 }
 
 FPrimaryAssetId UItemDefinition::GetPrimaryAssetId() const
@@ -17,4 +25,86 @@ FPrimaryAssetId UItemDefinition::GetPrimaryAssetId() const
 	return FPrimaryAssetId(GetFName(), PrimaryAssetType.AssetTypeId);
 }
 
+FText UItemDefinition::GetDisplayName() const
+{
+	if (ItemName.IsEmpty())
+	{
+		return FText::FromName(GetFName());
+	}
+
+	return ItemName;
+}
+
+FText UItemDefinition::GetItemDescription() const
+{
+	if (ItemDescription.IsEmpty())
+	{
+		return ItemShortDescription;
+	}
+
+	return ItemDescription;
+}
+
+FText UItemDefinition::GetItemShortDescription() const
+{
+	return ItemShortDescription;
+}
+
+FText UItemDefinition::GetItemRichDescription() const
+{
+	FText RawText = GetItemDescription();
+	if (RawText.IsEmpty())
+    {
+        return FText::GetEmpty();
+    }
+
+	// @TODO: Add rich text formatting support
+	// FText::Format(FText::FromString("<RichTextBlock>{0}</RichTextBlock>"), RawText);
+	return RawText;
+}
+
+void UItemDefinition::GetItemComponent(
+	EItemDataQueryResult& ExecResult,
+	UScriptStruct* ComponentType,
+	FItemComponentData& FoundComponent) const
+{
+	if (ComponentType == nullptr)
+	{
+		ExecResult = EItemDataQueryResult::NotFound;
+		return;
+	}
+
+	// @TODO: Custom K2 Node for this function actually
+}
+
+const FItemComponentData* UItemDefinition::GetItemComponent(const UScriptStruct* PropertyType)
+{
+	for (auto& ComponentInstance : ItemComponents)
+	{
+		if (ComponentInstance.GetScriptStruct() == PropertyType)
+		{
+			return ComponentInstance.GetPtr<FItemComponentData>();
+		}
+	}
+
+	return nullptr;
+}
+
+void UItemDefinition::GetAssetRegistryTags(FAssetRegistryTagsContext Context) const
+{
+	Super::GetAssetRegistryTags(Context);
+}
+
+#if WITH_EDITOR
+void UItemDefinition::PostLoad()
+{
+	Super::PostLoad();
+	AssetType_Reg = AssetType.AssetTypeId;
+}
+
+void UItemDefinition::PostSaveRoot(FObjectPostSaveRootContext ObjectSaveContext)
+{
+	AssetType_Reg = AssetType.AssetTypeId;
+}
+#endif
 #undef LOCTEXT_NAMESPACE
--- Source/ItemizationCoreRuntime/Public/Items/ItemDefinition.h
+++ Source/ItemizationCoreRuntime/Public/Items/ItemDefinition.h
@@ -6,8 +6,11 @@
 #include "ItemAssetTypeId.h"
 #include "ItemComponentData.h"
 #include "Engine/DataAsset.h"
+#include "Enums/EItemDataQueryResult.h"
 #include "StructUtils/InstancedStruct.h"
+
 #include "ItemDefinition.generated.h"
+
 
 /** Const data definition of a single item in the game. */
 UCLASS(BlueprintType, Blueprintable, Config=Game, Const)
@@ -22,6 +25,35 @@ public:
 	virtual FPrimaryAssetId GetPrimaryAssetId() const override;
 	//~ End UPrimaryDataAsset Interface
 
+	/** Returns the display of this item definition, if none is set, the class name will be used. */
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = Item)
+	FText GetDisplayName() const;
+
+	/** Returns the raw item description. Will fall back to the short description if none is set. */
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = Item)
+	FText GetItemDescription() const;
+
+	/** Returns the raw short description. */
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = Item)
+	FText GetItemShortDescription() const;
+
+	/** Returns the item description with rich text formatting. */
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = Item)
+	FText GetItemRichDescription() const;
+
+	/** Tries to find the item component data for the given type. */
+	UFUNCTION(BlueprintCallable, BlueprintPure=false, Category = Item, meta=(ExpandEnumAsExecs=ExecResult))
+	void GetItemComponent(EItemDataQueryResult& ExecResult, UScriptStruct* PropertyType, FItemComponentData& FoundComponent) const;
+
+	/** Returns the item component data as the given struct type. */
+	const FItemComponentData* GetItemComponent(const UScriptStruct* PropertyType);
+	template <typename T>
+	FORCEINLINE const T* GetItemComponent() const
+	{
+		static_assert(TIsDerivedFrom<T, FItemComponentData>::IsDerived, "T must be derived from FItemComponentData");
+		return GetItemComponent(T::StaticStruct());
+	}
+
 public:
 	/** An FName describing the type of this item. Should only be changed in very parent classes. */
 	UPROPERTY(VisibleDefaultsOnly, Category=AssetManager)
@@ -32,10 +64,36 @@ public:
 	 * This is used to group items in the inventory and should be unique for each item type.
 	 * This is different from the PrimaryAssetType, which is used for the AssetManager.
 	 */
-	UPROPERTY(VisibleDefaultsOnly, Category=AssetManager, AssetRegistrySearchable)
+	UPROPERTY(VisibleDefaultsOnly, Category=AssetManager)
 	FItemAssetTypeId AssetType;
 
-	/** */
-	UPROPERTY(EditDefaultsOnly, Category=Item, NoClear, meta=(ExcludeBaseStruct))
-	TArray<TInstancedStruct<FItemComponentData>> Components;
+#if WITH_EDITORONLY_DATA
+	/** Editor-only registry searchable asset type as an FName. */
+	UPROPERTY(AssetRegistrySearchable, DisplayName="Item Type")
+	FName AssetType_Reg = NAME_None;
+#endif
+	//~ Begin UObject Interface
+	virtual void GetAssetRegistryTags(FAssetRegistryTagsContext Context) const override;
+#if WITH_EDITOR
+	virtual void PostLoad() override;
+	virtual void PostSaveRoot(FObjectPostSaveRootContext ObjectSaveContext) override;
+#endif
+	//~ End UObject Interface
+
+protected:
+	/** The friendly name to be shown in the UI for this item. */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category=Display, AssetRegistrySearchable, DisplayName="Display Name")
+	FText ItemName;
+
+	/** The description of this item for summary information. */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category=Display, DisplayName = "Description")
+	FText ItemDescription;
+
+	/** The short description of this item for summary information. Mostly used for tooltips. */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category=Display, DisplayName = "Short Description")
+	FText ItemShortDescription;
+
+	/** List of item components that are attached to this item. */
+	UPROPERTY(EditDefaultsOnly, Category=Item, NoClear, meta=(ExcludeBaseStruct,ShowOnlyInnerProperties))
+	TArray<TInstancedStruct<FItemComponentData>> ItemComponents;
 };
