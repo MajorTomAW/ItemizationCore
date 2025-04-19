27312840304408202fc580330130fa5d28cc012f
MajorTomAW <tom.wrnr.private@gmail.com>
2025-04-19 16:30:39 +0200

    Added some shared (native) Item Trait Gameplay Tags.

---
+++ Source/ItemizationCoreRuntime/Public/ItemizationCoreTags.h
@@ -0,0 +1,35 @@
+// Copyright © 2025 MajorT. All Rights Reserved.
+
+#pragma once
+
+#include "NativeGameplayTags.h"
+
+#define ITEMIZATION_TAG(TagName) \
+	ITEMIZATIONCORERUNTIME_API extern FNativeGameplayTag TagName;
+
+namespace ItemizationCoreTags
+{
+	ITEMIZATION_TAG(TAG_ItemTrait)
+	ITEMIZATION_TAG(TAG_ItemTrait_AllowEmptyFinalStack)
+	ITEMIZATION_TAG(TAG_ItemTrait_AllowEmptyStack)
+	ITEMIZATION_TAG(TAG_ItemTrait_AllowItemSyncShare)
+	ITEMIZATION_TAG(TAG_ItemTrait_AllowQuickbarFocusForGameplayOnly)
+	ITEMIZATION_TAG(TAG_ItemTrait_AllowSwapSingleStack)
+	ITEMIZATION_TAG(TAG_ItemTrait_AlwaysCountForCollectionQuest)
+	ITEMIZATION_TAG(TAG_ItemTrait_AutoCombineStacks)
+	ITEMIZATION_TAG(TAG_ItemTrait_CacheFiringRateOnWeaponFire)
+	ITEMIZATION_TAG(TAG_ItemTrait_DisallowActivateOnQuickbarFocus)
+	ITEMIZATION_TAG(TAG_ItemTrait_DisallowDepositInStorageVault)
+	ITEMIZATION_TAG(TAG_ItemTrait_DisallowQuickbarFocus)
+	ITEMIZATION_TAG(TAG_ItemTrait_ForceIntoOverflow)
+	ITEMIZATION_TAG(TAG_ItemTrait_ForceQuickbarFocusWhenAdded)
+	ITEMIZATION_TAG(TAG_ItemTrait_ForceStayInOverflow)
+	ITEMIZATION_TAG(TAG_ItemTrait_HasDurability)
+	ITEMIZATION_TAG(TAG_ItemTrait_HideItemToast)
+	ITEMIZATION_TAG(TAG_ItemTrait_IgnoreRemoveAllInventoryItems)
+	ITEMIZATION_TAG(TAG_ItemTrait_InventorySizeLimited)
+	ITEMIZATION_TAG(TAG_ItemTrait_ShuffleTile)
+	ITEMIZATION_TAG(TAG_ItemTrait_SingleStack)
+	ITEMIZATION_TAG(TAG_ItemTrait_Transient)
+}
+
