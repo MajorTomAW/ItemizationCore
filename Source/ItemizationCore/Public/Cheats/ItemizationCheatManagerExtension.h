75f1dd79b80a99cd59facf3cb6e4dfd5ce5144f6
MajorTomAW <tom.wrnr.private@gmail.com>
2026-01-13 22:33:51 +0100

    added a cheat manager extension for giving (and removing) items

---
+++ Source/ItemizationCore/Public/Cheats/ItemizationCheatManagerExtension.h
@@ -0,0 +1,33 @@
+﻿// Author: Tom Werner (dc: majort), 2026 January
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ConsoleSettings.h"
+#include "GameFramework/CheatManager.h"
+#include "ItemizationCheatManagerExtension.generated.h"
+
+/** Cheats for the itemization core */
+UCLASS(NotBlueprintable)
+class UItemizationCheatManagerExtension final : public UCheatManagerExtension
+{
+	GENERATED_BODY()
+
+public:
+	UItemizationCheatManagerExtension();
+
+	//~ Begin UCheatManagerExtension Interface
+	virtual void AddedToCheatManager_Implementation() override;
+	//~ End UCheatManagerExtension Interface
+
+protected:
+	static void PopulateAutoCompleteEntries(TArray<FAutoCompleteCommand>& AutoCompleteCommands);
+
+	/**
+	 * Gives a specified item and count to the owning player.
+	 * @param ItemAssetId PrimaryAssetId and Name of the item.
+	 * @param Count The number of items to give. Default is 1.
+	 */
+	UFUNCTION(Exec)
+	void GiveItem(const FString& ItemAssetId, int32 Count = 1) const;
+};
